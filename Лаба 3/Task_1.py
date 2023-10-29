d8f984ed71cf699c085b72c3d42131080d894843
VoiceOf90s <60218217+VoiceOf90s@users.noreply.github.com>
2023-10-29 23:12:27 +0300

    Лабораторная работа №3

---
+++ Лаба 3/Task_1.py
@@ -0,0 +1,12 @@
+def items(a,b):
+    if b in a:
+        return a.index(b)
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item =items(items_list,find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
